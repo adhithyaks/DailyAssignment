f4fe64d5ed47dfe8c8f6c84a916b28ff219d9c2f
adhithyaks <90975559+adhithyaks@users.noreply.github.com>
2023-04-14 19:12:14 +0530

    Add files via upload

---
+++ day8/telephone/telephone/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+public class Telephone
+{
+    protected string _phonetype;
+        public string Phonetype
+    {
+
+        get
+        {
+            return _phonetype;
+        }
+        set
+        {
+            if (_phonetype != null) _phonetype = value;
+        }
+    }
+
+    public void Ring()
+    {
+        Console.WriteLine("Ringing the {0}", _phonetype);
+    }
+    public Telephone()
+    {
+
+    }
+
+    public Telephone(string phone)
+    {
+        _phonetype = phone;
+    }
+}
+public class ElectronicPhone : Telephone
+{
+     _phonetype = "Digital";
+}
---
+++ day8/telephone/telephone/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace phone
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ElectronicPhone phone = new ElectronicPhone();
+            phone.Ring();
+        }
+    }
+}
+
+
+    
