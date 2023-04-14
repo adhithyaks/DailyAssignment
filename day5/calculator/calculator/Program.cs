b078f0e78e4266ac94c72e06d5be1cfef1984ca0
adhithyaks <90975559+adhithyaks@users.noreply.github.com>
2023-04-14 15:35:12 +0530

    Add files via upload

---
+++ day5/calculator/calculator/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1, number2, ch;
+            Console.WriteLine("enter first number");
+            number1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter second number");
+            number2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("the available options are: ");
+            Console.WriteLine("1.addition\n 2.subtraction\n 3.multiplication\n 4.division");
+            Console.WriteLine("enter your choice");
+            ch = int.Parse(Console.ReadLine());
+
+            switch (ch)
+            {
+                case 1:
+                    Console.WriteLine("the sum of {0} and {1} is {2}", number1, number2, number1 + number2);
+                    break;
+                case 2:
+                    Console.WriteLine("the subtraction of {0} and {1} is {2}", number1, number2, number1 - number2);
+                    break;
+                case 3:
+                    Console.WriteLine("the multiplication of {0} and {1} is {2}", number1, number2, number1 * number2);
+                    break;
+                case 4:
+                    Console.WriteLine("the division of {0} and {1} is {2}", number1, number2, number1 / number2);
+                    break;
+                default:
+                    Console.WriteLine("input incorrect choice\n");
+                    break;
+
+            }
+
+        }
+    }
+}
---
+++ day5/menu/menu/Account.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace AccountsDemoApp
+{
+    class Account
+    {
+        public int Id;
+        public string Name;
+        public decimal Balance;
+        public Account()
+        {
+            Console.WriteLine("Object Created");
+        }
+        ~Account()
+        {
+            Console.WriteLine("Object Destroyed");
+        }
+        public void CreateAccount(int id, string name, decimal balance)
+        {
+            Id = id;
+            Name = name;
+            Balance = balance;
+        }
+    
+         public void DisplayInfo()
+         {
+            Console.WriteLine("***** Account Information *****");
+            Console.WriteLine("Account Id:" + Id);
+            Console.WriteLine("Name:" + Name);
+            Console.WriteLine("Opening Balance:" + Balance);
+          }
+         public void Deposit(decimal amount)
+         {
+            Balance += amount;
+         }
+        public void Withdraw(decimal amount)
+        {
+            if (Balance - amount < 500)
+                Console.WriteLine("Insufficient Balance");
+            else
+                this.Balance -= amount;
+        }
+     }
+}
