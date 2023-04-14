af5e8fefa4ded5407b28d781690bce8ea9d9e090
adhithyaks <90975559+adhithyaks@users.noreply.github.com>
2023-04-14 15:34:12 +0530

    Add files via upload

---
+++ day4/sorting1/sorting1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sorting1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ar = { 56, 44, 3, 24, 90, 11 };
+            int temp;
+            for(int i=0; i<6; i++)
+            {
+                Console.WriteLine(ar[i] + "\t");
+            }
+            Console.WriteLine();
+            for (int j=0; j<ar.Length-1;j++)
+            {
+                for (int i=j;i<ar.Length-1;i++)
+                {
+                    if (ar[j] > ar[i+1])
+                    {
+                        temp = ar[j];
+                        ar[j] = ar[i+1];
+                        ar[i+1] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("sorted arrray is: ");
+            for(int i= 0; i<6; i++)
+            {
+                Console.WriteLine(ar[i] + "\t");
+            }
+            Console.WriteLine() ;
+        }
+    }
+}
---
+++ day4/swapping2/swapping2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+    public class swapp
+    {
+        static void Main(string[] args)
+        {
+            int number1, number2, temp;
+            Console.WriteLine("enter the first number");
+            number1=int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the second number");
+            number2=int.Parse(Console.ReadLine());
+             temp = number1;
+            number1 = number2;
+            number2 = temp; 
+            Console.WriteLine("after swapping");
+            Console.WriteLine("first number is: " + number1);
+            Console.WriteLine("second number is: " + number2);
+            
+        
+        }
+    }
