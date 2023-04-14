7ae3c23c9745a19581c0cc989678ad0fbc78e8c9
adhithyaks <90975559+adhithyaks@users.noreply.github.com>
2023-04-14 15:19:08 +0530

    Add files via upload

---
+++ day 2/area of circle/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Syst
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int r;
+            double area;
+            Console.WriteLine("Enter Radius:");
+            r = int.Parse(Console.ReadLine());
+            area = circle(r);
+            Console.WriteLine("area of circle having radius {0} is {1}", r, area);
+
+        }
+        static double circle(double r)
+
+        {
+            double area ;
+            area = 3.14 * r * r;
+            return area;
+        }
+    }
+}
---
+++ day 2/greaternumber/greaternumber/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace greaternumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("enter the number");
+            n = int.Parse(Console.ReadLine());
+            int result = absolutedifference(n);
+            Console.WriteLine("THE RESULT = {0}", result);
+
+
+
+        }
+        static int absolutedifference(int n)
+        {
+            if (n > 51)
+            {
+                int res = (n - 51) * 3; 
+                return res;
+            }
+            else
+            {
+                int res1 = (51 - n);
+                return res1;
+            }
+        }
+    }
+}
