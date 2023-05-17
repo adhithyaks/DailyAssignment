1cabb031f69d682942a73b05da514ffe2fd7deec
adhithyaks <90975559+adhithyaks@users.noreply.github.com>
2023-05-17 09:38:54 +0530

    Add files via upload

---
+++ day24/EF6/EF6/Program.cs
@@ -0,0 +1,24 @@
+﻿using EF6.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (var ctx = new SchoolContext())
+            {
+                var stud = new Student() { StudentName = "Bill" };
+
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+            }
+        }
+    }
+}
---
+++ day24/EFDEMO2/EFDEMO2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFDEMO2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //INSERT
+            /*using (var context = new SchoolDBEntities())
+            {
+                var std = new Student()
+                {
+                    FirstName = "Bill",
+                    LastName = "Gates"
+                };
+                context.Students.Add(std);
+
+                context.SaveChanges();
+            }
+            //UPDATE
+            using (var context = new SchoolDBEntities())
+            {
+                var std = context.Students.First<Student>();
+                std.FirstName = "Steve";
+                context.SaveChanges();
+            }*/
+            //DELETE
+            using (var context = new SchoolDBEntities())
+            {
+                var std = context.Students.First<Student>();
+                context.Students.Remove(std);
+
+                context.SaveChanges();
+            }
+
+        }
+    }
+}
