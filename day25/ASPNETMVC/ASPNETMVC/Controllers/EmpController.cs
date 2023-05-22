5d2517da26576b5594371267039f8cc2fd1c81f4
adhithyaks <90975559+adhithyaks@users.noreply.github.com>
2023-05-22 23:19:13 +0530

    Add files via upload

---
+++ day25/ASPNETMVC/ASPNETMVC/Controllers/EmpController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASPNETMVC.Controllers
+{
+    public class EmpController : Controller
+    {
+        public string Display()
+        {
+
+            return "hello vc code";
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
