bf0d35acf0ff5f253aade743a764fafed77e1b98
adhithyaks <90975559+adhithyaks@users.noreply.github.com>
2023-05-22 23:31:55 +0530

    Add files via upload

---
+++ day27(A)/Myfirstcorevs/Myfirstcorevs/Pages/firstpage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Myfirstcorevs.Pages
+{
+    public class firstpageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
