df4f207a6cf56355c9a0b5b3c71b426d7885c4f0
adhithyaks <90975559+adhithyaks@users.noreply.github.com>
2023-05-29 09:31:30 +0530

    Add files via upload

---
+++ day41/Abstract/Abstract/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+abstract class Book
+{
+     string title;
+
+    public abstract void setTitle(string s);
+
+    public string getTitle()
+    {
+        return title;
+    }
+}
+
+class MyBook : Book
+{
+    public override void setTitle(string s)
+    {
+        Console.WriteLine("the title is :{0}",s);
+    }
+}
+
+public class Program
+{
+     static void Main(string[] args)
+    {
+        
+        Console.WriteLine("enter the book name : ");
+        string str=Console.ReadLine();
+        MyBook book = new MyBook();   
+
+
+        book.setTitle(str);
+    }
+}
