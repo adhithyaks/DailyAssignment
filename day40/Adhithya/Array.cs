53e9e1abdfb606a7c0b28d361d93b9cebd06084a
adhithyaks <90975559+adhithyaks@users.noreply.github.com>
2023-05-29 09:30:25 +0530

    Add files via upload

---
+++ day40/Adhithya/Array.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+
+
+class Solution
+        {
+            static void Main()
+            {
+                int n = Convert.ToInt32(Console.ReadLine());
+                int[] arr = new int[n];
+
+                for (int i = 0; i < n; i++)
+                {
+                    arr[i] = Convert.ToInt32(Console.ReadLine());
+                }
+
+                foreach (int num in arr)
+                {
+                    Console.WriteLine(num);
+                }
+            }
+        }
+    }
+    
+
---
+++ day40/Adhithya/PowerOf.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerOf
+{
+    class Inner
+    {
+        private class Private
+        {
+            public bool Power(int num)
+            {
+                if (num == 0)
+                    return false;
+
+                while (num != 1)
+                {
+                    if (num % 2 != 0)
+                        
+                        return false;
+
+                    num = num / 2;
+                }
+                return true;
+            }
+        }
+
+        class Solution
+        {
+
+            static void Main(string[] args)
+            {
+                Inner.Private temp = new Inner.Private();
+
+
+                Console.WriteLine("Enter a number");
+
+                int num = int.Parse(Console.ReadLine());
+                if (temp.Power(num))
+                {
+
+                    Console.WriteLine("{0} is a power of 2", num);
+                }
+
+                else
+
+                    Console.WriteLine("{0} is not a power of 2", num);
+
+            }
+
+        }
+
+    }
+
+}
+        
