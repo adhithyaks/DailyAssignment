862ff444396a48e803b5a3500567789d3bab4cf7
adhithyaks <90975559+adhithyaks@users.noreply.github.com>
2023-06-08 17:30:31 +0530

    Add files via upload

---
+++ DAY49/CCJUNE1QN1/CODECHALLENGEDAY1QN1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+
+class Program
+
+{
+
+    static void Main(string[] args)
+
+    {
+
+        int lineNumber = 1;
+
+        while (true)
+
+        {
+
+            string line = Console.ReadLine();
+
+            if (line == null)
+
+                break;
+
+            Console.WriteLine($"{lineNumber} {line}");
+
+            lineNumber++;
+
+        }
+
+    }
+
+}
+
