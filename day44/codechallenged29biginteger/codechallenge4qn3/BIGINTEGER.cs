02379c745c288f5662b16d9e5f5ce24953bc5dd6
adhithyaks <90975559+adhithyaks@users.noreply.github.com>
2023-06-08 17:20:26 +0530

    Add files via upload

---
+++ day44/codechallenged29biginteger/codechallenge4qn3/BIGINTEGER.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+using System.Linq;
+
+using System.Numerics;
+
+using System.Text;
+
+using System.Threading.Tasks;
+
+namespace QSTN3
+
+{
+
+    internal class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            Console.WriteLine("Enter 1 number");
+
+            BigInteger b1 = BigInteger.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter 2 number");
+
+            BigInteger b2 = BigInteger.Parse(Console.ReadLine());
+
+            BigInteger b3 = b1 + b2;
+
+            Console.WriteLine("Sum:" + b3);
+
+            BigInteger b4 = b1 * b2;
+
+            Console.WriteLine("Multiplication:" + b4);
+
+        }
+
+    }
+
+}
+
